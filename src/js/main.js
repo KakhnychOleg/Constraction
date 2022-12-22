c88271cb55169264d4dee9c11693333f030095a9
Oleg <kakhnych.oleg@gmail.com>
2022-12-23 00:24:45 +0200

    added slider

--- src/js/main.js
+++ src/js/main.js
@@ -15,24 +15,53 @@ function onEntry(entry) {
    observer.observe(elm);
  }
 
+$(document).ready(function () {
+  // Mobile menu
+  $('.mobile div').on('click', () => {
+    $('.mobile div').toggleClass('active');
+    $('.mobile__nav').toggleClass('open');
+    $('.mobile__list').toggleClass('show');
+    $('html').toggleClass('hidden');
+  });
+  for (let a = 1; a <= $(".mobile__item").length; a++){
+    $(".mobile__item:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");   
+  };
 
+  // Slider testimonials
+  $('.reviews-slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    pauseOnHover: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
- $('.mobile div').on('click', () => {
-  $('.mobile div').toggleClass('active');
-  $('.mobile nav').toggleClass('open');
-  $('.mobile nav ul').toggleClass('show');
-});
-for (let a = 1; a <= $(".mobile ul li").length; a++){
-  $(".mobile ul li:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");   
-}
-
-
-
- $('.reviews-slider').slick({
-  dots: true,
-  infinite: true,
-  autoplay: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
+  // Accordion
+  $('.accordion__title').click(function(event) {
+    if($('.accordion').hasClass('one')){
+      $('.accordion__title').not($(this)).removeClass('active');
+      $('.accordion__text').not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass('active').next().slideToggle(300)
+  });
 });
