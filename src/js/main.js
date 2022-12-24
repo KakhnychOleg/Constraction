f70a9563519999f8feb4b0c659263db05c4d6ede
Oleg <kakhnych.oleg@gmail.com>
2022-12-24 11:28:50 +0300

    added contact form

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,3 @@
-function onEntry(entry) {
-   entry.forEach(change => {
-     if (change.isIntersecting) {
-      change.target.classList.add('element-show');
-     }
-   });
- }
- 
- let options = {
-   threshold: [0.5] };
- let observer = new IntersectionObserver(onEntry, options);
- let elements = document.querySelectorAll('.element-animation');
- 
- for (let elm of elements) {
-   observer.observe(elm);
- }
-
 $(document).ready(function () {
   // Mobile menu
   $('.mobile div').on('click', () => {
@@ -53,4 +36,58 @@ console.log(currentSlide);
     }
     $(this).toggleClass('active').next().slideToggle(300)
   });
+
+  // Contact-checkbox
+  $(document).on('click', '.checkbox', function(event) {
+    if($(this).hasClass('active-check')){
+      $(this).find('input').prop('checked',false);
+      $('.checkbox[input]').css('opacity', '0');
+    }else {
+      $(this).find('input').prop('checked',true);
+      $('.checkbox[input]').css('opacity', '1');
+    }
+    $(this).toggleClass('active-check');
+
+    return false;
+  });
+
+
 });
+
+// Animation
+function onEntry(entry) {
+   entry.forEach(change => {
+     if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+     }
+   });
+ }
+ 
+ let options = {
+   threshold: [0.5] };
+ let observer = new IntersectionObserver(onEntry, options);
+ let elements = document.querySelectorAll('.element-animation');
+ 
+ for (let elm of elements) {
+   observer.observe(elm);
+ }
+
+
+
+//  sca
+let inputs = document.querySelectorAll('.input__file');
+    Array.prototype.forEach.call(inputs, function (input) {
+      let label = input.nextElementSibling,
+        labelVal = label.querySelector('.input__file-button-text').innerText;
+  
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+  
+        if (countFiles)
+          label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+          label.querySelector('.input__file-button-text').innerText = labelVal;
+      });
+    });
