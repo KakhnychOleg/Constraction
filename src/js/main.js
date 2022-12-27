e33452160889821d77b9d527de510b380660bb1a
Oleg <kakhnych.oleg@gmail.com>
2022-12-27 17:25:05 +0200

    delete animation effect

--- src/js/main.js
+++ src/js/main.js
@@ -58,22 +58,22 @@ $(document).ready(function () {
 });
 
 // Animation
-function onEntry(entry) {
-   entry.forEach(change => {
-     if (change.isIntersecting) {
-      change.target.classList.add('element-show');
-     }
-   });
- }
+// function onEntry(entry) {
+//    entry.forEach(change => {
+//      if (change.isIntersecting) {
+//       change.target.classList.add('element-show');
+//      }
+//    });
+//  }
  
- let options = {
-   threshold: [0.1] };
- let observer = new IntersectionObserver(onEntry, options);
- let elements = document.querySelectorAll('.element-animation');
+//  let options = {
+//    threshold: [0.1] };
+//  let observer = new IntersectionObserver(onEntry, options);
+//  let elements = document.querySelectorAll('.element-animation');
  
- for (let elm of elements) {
-   observer.observe(elm);
- }
+//  for (let elm of elements) {
+//    observer.observe(elm);
+//  }
 
 //  sca
 let inputs = document.querySelectorAll('.input__file');
